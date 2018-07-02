25325e8a10af956f0941001329d05349d264ad79
TlStephen2011 <tlstephen11@gmail.com>
2018-07-02 17:53:45 +0200

    Updated setup_script. Removed adding z to .zshrc

--- setup_script.sh
+++ setup_script.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
 #grab oh-my-zsh
--c curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #setup .vimrc
 echo "set nu\nset relativenumber\nset mouse=a\nset autoindent\nsyntax on" > ~/.vimrc && source ~/.vimrc
-#add z to .zshrc
-awk 'plugins=( { print; print "\nz"; next }1' ~/.vimrc && source ~/.zshrc
 #get ssh key and copy it clipboard
 ssh-keygen
 ls ~/.ssh
